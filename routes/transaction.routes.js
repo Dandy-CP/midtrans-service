25c6dc9105a3165388f1392ba283f39b45bb87f0
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-10 17:08:20 +0700

    add check transaction route and implement validator

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,10 +1,23 @@
 import express from "express";
+import { query } from "express-validator";
+import validateTransaction from "../middleware/validateTransaction.js";
 import PostCreateTransaction from "../controller/createTransaction.js";
+import CheckStatusTransaction from "../controller/checkStatusTransaction.js";
 
 const transactionRoutes = (app) => {
   const router = express.Router();
 
-  router.post("/transaction", PostCreateTransaction);
+  router.post(
+    "/transaction",
+    validateTransaction("createTransaction"),
+    PostCreateTransaction
+  );
+
+  router.post(
+    "/check-transaction",
+    query("order_id").notEmpty(),
+    CheckStatusTransaction
+  );
 
   app.use("/", router);
 };
