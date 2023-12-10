9a2116a2b157edce3ae1eeec8af1ec616cf41c70
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-10 22:06:16 +0700

    add route expire, refund and cancel transaction

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -3,6 +3,9 @@ import { query } from "express-validator";
 import validateTransaction from "../middleware/validateTransaction.js";
 import PostCreateTransaction from "../controller/createTransaction.js";
 import CheckStatusTransaction from "../controller/checkStatusTransaction.js";
+import ExpireTransaction from "../controller/expireTransaction.js";
+import RefundTransaction from "../controller/refundTransaction.js";
+import CancelTransaction from "../controller/cancelTransaction.js";
 
 const transactionRoutes = (app) => {
   const router = express.Router();
@@ -17,6 +20,24 @@ const transactionRoutes = (app) => {
     "/check-transaction",
     query("order_id").notEmpty(),
     CheckStatusTransaction
+  );
+
+  router.post(
+    "/expire-transaction",
+    query("order_id").notEmpty(),
+    ExpireTransaction
+  );
+
+  router.post(
+    "/refund-transaction",
+    validateTransaction("refundTransaction"),
+    RefundTransaction
+  );
+
+  router.post(
+    "/cancel-transaction",
+    query("order_id").notEmpty(),
+    CancelTransaction
   );
 
   app.use("/", router);
