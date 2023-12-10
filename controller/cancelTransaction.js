ced955466221561d3a52c9052dfaaa22cae2b334
Dandy Candra <dandycandrapratama@gmail.com>
2023-12-10 22:04:38 +0700

    add controller cancel transaction

---
+++ controller/cancelTransaction.js
@@ -0,0 +1,26 @@
+import { validationResult } from "express-validator";
+import snap from "../utils/snapMidtrans.js";
+
+const CancelTransaction = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
+  }
+
+  const { order_id } = req.query;
+
+  snap.transaction
+    .cancel(order_id)
+    .then((response) => {
+      return res.status(200).send(response);
+    })
+    .catch((error) => {
+      return res
+        .status(Number(error.httpStatusCode))
+        .send({ message: error.message, status: Number(error.httpStatusCode) });
+    });
+};
+
+export default CancelTransaction;
